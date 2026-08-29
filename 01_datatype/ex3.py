b92cc78feb90686b378608631c962ab60fab4202
정유건 <yougun0727@gmail.com>
2026-08-29 12:01:28 +0900

    ex3.py

---
+++ 01_datatype/ex3.py
@@ -0,0 +1,28 @@
+# 불리언
+# true, false
+
+a = True
+print(a, type(a))
+
+print(2 < 3)
+print(2 > 3)
+print(2 == 3)
+print(2 != 3)
+
+print("apple" > "banana")
+
+#bool()
+print(bool(3))
+print(bool(0))
+print(bool("hello"))
+print(bool([10]))
+print(bool([]))
+
+# None 자료형
+a = None
+print(a, type(a))
+print(bool(a))
+
+if a is None:
+    print("값이 없습니다.")
+    
